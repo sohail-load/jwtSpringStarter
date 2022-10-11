aef70b3b77d8d32777f9b4db10d1e8b58f733292
sohail-load <asohail180@gmail.com>
2022-10-12 01:44:14 +0530

    Added Spring Security Configuration classes

--- src/main/java/com/mystarter/StarterSpring/controller/UserController.java
+++ src/main/java/com/mystarter/StarterSpring/controller/UserController.java
@@ -3,9 +3,12 @@ package com.mystarter.StarterSpring.controller;
 import com.mystarter.StarterSpring.model.User;
 import com.mystarter.StarterSpring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
 
 @RestController
 public class UserController {
@@ -13,8 +16,23 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PostConstruct
+    public void initRolesAndUsers() {
+        userService.initRolesAndUsers();
+    }
+
     @PostMapping({"/registerNewUser"})
     public User registerNewUser(@RequestBody User user) {
         return userService.registerNewUser(user);
+    }
+
+    @GetMapping({"/forAdmin"})
+    public String forAdmin() {
+        return "This URL is only accessible to admin";
+    }
+
+    @GetMapping({"/forUser"})
+    public String forUser() {
+        return "This URL is only accessible to user";
     }
 }
--- src/main/java/com/mystarter/StarterSpring/service/UserService.java
+++ src/main/java/com/mystarter/StarterSpring/service/UserService.java
@@ -1,16 +1,56 @@
 package com.mystarter.StarterSpring.service;
 
+import com.mystarter.StarterSpring.dao.RoleDao;
 import com.mystarter.StarterSpring.dao.UserDao;
+import com.mystarter.StarterSpring.model.Role;
 import com.mystarter.StarterSpring.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public class UserService {
     @Autowired
     private UserDao userDao;
 
+    @Autowired
+    private RoleDao roleDao;
+
     public User registerNewUser(User user) {
         return userDao.save(user);
+    }
+
+    public void initRolesAndUsers() {
+        Role adminRole = new Role();
+        adminRole.setRoleName("Admin");
+        adminRole.setRoleDescription("Role for admin");
+        roleDao.save(adminRole);
+
+        Role userRole = new Role();
+        userRole.setRoleName("User");
+        userRole.setRoleDescription("Default role for new user");
+        roleDao.save(userRole);
+
+        User adminUser = new User();
+        adminUser.setUserFirstName("admin");
+        adminUser.setUserLastName("admin");
+        adminUser.setUserName("admin123");
+        adminUser.setUserPassword("password");
+        Set<Role> adminRoles = new HashSet<>();
+        adminRoles.add(adminRole);
+        adminUser.setRole(adminRoles);
+        userDao.save(adminUser);
+
+        User user = new User();
+        user.setUserFirstName("Amy");
+        user.setUserLastName("Adams");
+        user.setUserName("amy123");
+        user.setUserPassword("password");
+        Set<Role> userRoles = new HashSet<>();
+        userRoles.add(userRole);
+        user.setRole(userRoles);
+        userDao.save(user);
     }
 }
